3c70f7d845022026d1ad8079c3fc2adf86f4a6a8
Sebastien Carlier <s.carlier@epixode.fr>
2017-01-01 23:33:04 +0000

    move workspace view into views.js

--- src/views.js
+++ src/views.js
@@ -94,6 +94,78 @@ export const Plain = EpicComponent(self => {
   };
 });
 
-export const preventDefault = function (event) {
+const preventDefault = function (event) {
   event.preventDefault();
 };
+
+export const View = actions => EpicComponent(self => {
+
+  self.state = {dragging: false};
+
+  const onKeyChange = function (index, direction) {
+    const {key} = self.props.workspace;
+    self.props.dispatch({type: actions.keyChange, index, direction});
+  };
+
+  const onMouseDown = function (cipherIndex, charIndex) {
+    self.setState({dragging: true});
+    self.props.dispatch({type: actions.setPlainWordPosition, cipherIndex, charIndex});
+  };
+
+  const onHover = function(cipherIndex, charIndex) {
+    if (self.state.dragging) {
+      self.props.dispatch({type: actions.setPlainWordPosition, cipherIndex, charIndex});
+    }
+  };
+
+  const onMouseUp = function() {
+    self.setState({dragging: false});
+  };
+
+  self.render = function () {
+    const {score, task, workspace, dispatch} = self.props;
+    const {key, keyWithWord, wordCharIndex, wordCipherIndex} = workspace;
+    const {ciphers, plainWord} = task;
+    const wordStartIndex = Math.max(0, Math.min(wordCharIndex, key.length - plainWord.length));
+    return (
+      /* TODO how to cach mouse up in entire document? */
+      /* preventDefault is called because browsers default to a visual dragging of HTML elements */
+      <div onMouseUp={onMouseUp} onMouseMove={preventDefault}>
+        <table className="keyTable">
+          <tr>
+            {key.map(function(keyValue, keyIndex) {
+              if(wordCipherIndex !== null && keyIndex >= wordStartIndex && keyIndex < wordStartIndex + plainWord.length) {
+                return <td key={keyIndex}></td>;
+              }
+              return <td key={keyIndex}><KeyButton index={keyIndex} direction="1" onChange={onKeyChange} /></td>;
+            })}
+          </tr>
+          <tr>
+            {keyWithWord.map(function(keyValue, keyIndex) {
+              return <td key={keyIndex}>{keyValue.value}</td>;
+            })}
+          </tr>
+          <tr>
+            {key.map(function(keyValue, keyIndex) {
+              if(wordCipherIndex !== null && keyIndex >= wordStartIndex && keyIndex < wordStartIndex + plainWord.length) {
+                return <td key={keyIndex}></td>;
+              }
+              return <td key={keyIndex}><KeyButton index={keyIndex} direction="-1" onChange={onKeyChange} /></td>;
+            })}
+          </tr>
+        </table>
+        <div className="ciphersAndPlains">
+          {ciphers.map(function(cipherValue, cipherIndex) {
+            return (
+              <div>
+                <Cipher index={cipherIndex} value={cipherValue} onHover={onHover} />
+                <Plain cipherIndex={cipherIndex} cipherValue={cipherValue} keyWithWord={keyWithWord} wordCharIndex={wordCharIndex} wordCipherIndex={wordCipherIndex} plainWord={plainWord} onMouseDown={onMouseDown} onHover={onHover} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  };
+
+});
--- src/workspace.js
+++ src/workspace.js
@@ -4,7 +4,7 @@ import {include, defineAction, addReducer} from 'epic-linker';
 import WorkspaceBuilder from 'alkindi-task-lib/simple_workspace';
 
 import {generateKeyWithWord, ALPHABET_SIZE} from './utils';
-import {KeyButton, Cipher, Plain, preventDefault} from './views';
+import {View} from './views';
 
 export default function* (deps) {
 
@@ -47,79 +47,7 @@ export default function* (deps) {
     return {...workspace, keyWithWord};
   };
 
-  const View = EpicComponent(self => {
-
-    self.state = {dragging: false};
-
-    const onKeyChange = function (index, direction) {
-      const {key} = self.props.workspace;
-      self.props.dispatch({type: deps.keyChange, index, direction});
-    };
-
-    const onMouseDown = function (cipherIndex, charIndex) {
-      self.setState({dragging: true});
-      self.props.dispatch({type: deps.setPlainWordPosition, cipherIndex, charIndex});
-    };
-
-    const onHover = function(cipherIndex, charIndex) {
-      if (self.state.dragging) {
-        self.props.dispatch({type: deps.setPlainWordPosition, cipherIndex, charIndex});
-      }
-    };
-
-    const onMouseUp = function() {
-      self.setState({dragging: false});
-    };
-
-    self.render = function () {
-      const {score, task, workspace, dispatch} = self.props;
-      const {key, keyWithWord, wordCharIndex, wordCipherIndex} = workspace;
-      const {ciphers, plainWord} = task;
-      const wordStartIndex = Math.max(0, Math.min(wordCharIndex, key.length - plainWord.length));
-      return (
-        /* TODO how to cach mouse up in entire document? */
-        /* preventDefault is called because browsers default to a visual dragging of HTML elements */
-        <div onMouseUp={onMouseUp} onMouseMove={preventDefault}>
-          <table className="keyTable">
-            <tr>
-              {key.map(function(keyValue, keyIndex) {
-                if(wordCipherIndex !== null && keyIndex >= wordStartIndex && keyIndex < wordStartIndex + plainWord.length) {
-                  return <td key={keyIndex}></td>;
-                }
-                return <td key={keyIndex}><KeyButton index={keyIndex} direction="1" onChange={onKeyChange} /></td>;
-              })}
-            </tr>
-            <tr>
-              {keyWithWord.map(function(keyValue, keyIndex) {
-                return <td key={keyIndex}>{keyValue.value}</td>;
-              })}
-            </tr>
-            <tr>
-              {key.map(function(keyValue, keyIndex) {
-                if(wordCipherIndex !== null && keyIndex >= wordStartIndex && keyIndex < wordStartIndex + plainWord.length) {
-                  return <td key={keyIndex}></td>;
-                }
-                return <td key={keyIndex}><KeyButton index={keyIndex} direction="-1" onChange={onKeyChange} /></td>;
-              })}
-            </tr>
-          </table>
-          <div className="ciphersAndPlains">
-            {ciphers.map(function(cipherValue, cipherIndex) {
-              return (
-                <div>
-                  <Cipher index={cipherIndex} value={cipherValue} onHover={onHover} />
-                  <Plain cipherIndex={cipherIndex} cipherValue={cipherValue} keyWithWord={keyWithWord} wordCharIndex={wordCharIndex} wordCipherIndex={wordCipherIndex} plainWord={plainWord} onMouseDown={onMouseDown} onHover={onHover} />
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      );
-    };
-
-  });
-
-  yield include(WorkspaceBuilder({init, dump, load, update, View}));
+  yield include(WorkspaceBuilder({init, dump, load, update, View: View(deps)}));
 
   /*
     Add reducers for workspace actions and any needed sagas below:
