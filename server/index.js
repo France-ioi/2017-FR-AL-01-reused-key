1d51f4cd8047ace8f5b959a52826ac071490167f
Sebastien Carlier <s.carlier@epixode.fr>
2017-01-16 23:15:52 +0000

    add grantHint API

--- server/index.js
+++ server/index.js
@@ -4,14 +4,14 @@ const alkindiTaskServer = require('alkindi-task-lib/server');
 alkindiTaskServer({
   webpackConfig: require('../webpack.config.js'),
   generate: require('./generate'),
-  gradeAnswer
+  gradeAnswer,
+  grantHint
 });
 
 function gradeAnswer (full_task, task, answer, callback) {
   const {secretKey} = full_task;
   const {hints} = task;
   const {key} = answer;
-  const nHints = Object.keys(hints).length;
   let nCorrect = 0;
   secretKey.forEach(function (value, index) {
     if (value === key[index]) {
@@ -21,8 +21,26 @@ function gradeAnswer (full_task, task, answer, callback) {
   const is_full_solution = nCorrect === secretKey.length;
   const is_solution = is_full_solution;  // could be nCorrect > 0
   const feedback = is_full_solution;
-  const score = is_full_solution ? Math.max(0, 150 - nHints * 20) : 0;
+  const highestPossibleScore = getHighestPossibleScore(hints);
+  const score = is_full_solution ? highestPossibleScore : 0;
   callback(null, {
     feedback, score, is_solution, is_full_solution
   });
+}
+
+function grantHint (full_task, task, query) {
+  const {keyIndex} = query;
+  const {secretKey} = full_task;
+  if (typeof keyIndex !== 'number' || keyIndex < 0 || keyIndex >= secretKey.length) {
+    return {success: false};
+  }
+  // Update task in-place is it is freshly loaded JSON.
+  task.hints[keyIndex] = secretKey[keyIndex];
+  task.highestPossibleScore = getHighestPossibleScore(task.hints);
+  return {success: true, task: task};
+};
+
+function getHighestPossibleScore (hints) {
+  const nHints = Object.keys(hints).length;
+  return Math.max(0, 150 - nHints * 20);
 }
